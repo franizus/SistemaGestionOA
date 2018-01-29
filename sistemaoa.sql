e9e0758a8fdc3dadf86f15ba2035b026c2eae003
Unknown <franizus@live.com>
2018-01-28 22:17:59 -0500

    updated sql script

--- sistemaoa.sql
+++ sistemaoa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2018 at 01:14 AM
+-- Generation Time: Jan 29, 2018 at 04:17 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -25,54 +25,389 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `idAdministrador` int(11) NOT NULL,
+  `nombreAdmin` varchar(50) NOT NULL,
+  `usuarioAdmin` varchar(15) NOT NULL,
+  `pwAdmin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `administrador`
+--
+
+INSERT INTO `administrador` (`idAdministrador`, `nombreAdmin`, `usuarioAdmin`, `pwAdmin`) VALUES
+(1, 'Francisco Izurieta', 'admin', '$2y$10$nXfCxVyPD5M8nTsPR3Dk3.tBDBY2WZKrQqFuKXk7pGy/DjPkjNIKC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `idCarrera` int(11) NOT NULL,
+  `nombreCarrera` varchar(100) NOT NULL,
+  `idFacultad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `carrera`
+--
+
+INSERT INTO `carrera` (`idCarrera`, `nombreCarrera`, `idFacultad`) VALUES
+(1, 'Física', 1),
+(2, 'Matemáticas', 1),
+(3, 'Ingeniería Matemática', 1),
+(4, 'Ingeniería en Ciencias Económicas y Financieras', 1),
+(5, 'Ingeniería Empresarial', 2),
+(6, 'Ingeniería de la Producción', 2),
+(7, 'Ingeniería Civil', 3),
+(8, 'Ingeniería Ambiental', 3),
+(9, 'Ingeniería Eléctrica', 4),
+(10, 'Ingeniería en Electrónica y Control', 4),
+(11, 'Ingeniería en Electrónica y Redes de Información', 4),
+(12, 'Ingeniería en Electrónica y Telecomunicaciones', 4),
+(13, 'Ingeniería en Geología\r\n', 5),
+(14, 'Ingeniería en Petróleos', 5),
+(15, 'Ingenieria Mecanica', 6),
+(16, 'Ingeniería Agroindustrial', 7),
+(17, 'Ingeniería Química', 7),
+(18, 'Ingeniería en Software', 8),
+(19, 'Ingeniería en Computación', 8),
+(20, 'Ingeniería en Sistemas Informaticos y de Computacion', 8),
+(21, 'Tecnología en Electrónica y Telecomunicaciones', 9),
+(22, 'Tecnología en Análisis de Sistemas Informáticos', 9),
+(23, 'Tecnología en Electromecánica', 9),
+(24, 'Tecnología en Agua y Saneamiento Ambiental', 9),
+(25, 'Maestría en Física', 1),
+(26, 'Maestría en Sistemas de Gestión Integrados', 2),
+(27, 'Maestría en Gestión de Talento Humano', 2),
+(28, 'Maestría en Ciencias de Ingeniería Eléctrica', 4),
+(29, 'Maestría en Conectividad y Redes de Telecomunicaciones', 4),
+(30, 'Maestría en Automatización y Control Electrónico Industrial', 4),
+(31, 'Maestría en Administración de Negocios Eléctricos', 4),
+(32, 'Maestría en Ingeniería Eléctrica en Distribución', 4),
+(33, 'Maestría en Redes Eléctricas Inteligentes', 4),
+(34, 'Maestria en Mecatronica y Robotica', 6),
+(35, 'Maestria en Sistemas Automotrices', 6),
+(36, 'Maestria en Diseño y Simulacion', 6),
+(37, 'Programa Doctoral en Ciencias de la Mecanica', 6),
+(38, 'Maestría y Especialista en Gestión de las Comunicaciones y Tecnología de la Información', 8),
+(39, 'Maestría en Ciencias de la Computación', 8),
+(40, 'Maestría en Sistemas de Información', 8),
+(41, 'Doctorado en Informática', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `idComentario` int(11) NOT NULL,
+  `detalleComent` text NOT NULL,
+  `idOA` int(11) NOT NULL,
+  `idProfesor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `idDepartamento` int(11) NOT NULL,
+  `nombreDepartamento` varchar(100) NOT NULL,
+  `idFacultad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `departamento`
+--
+
+INSERT INTO `departamento` (`idDepartamento`, `nombreDepartamento`, `idFacultad`) VALUES
+(1, 'Departamento de Física (DF)', 1),
+(2, 'Departamento de Matemática (DM)', 1),
+(3, 'Departamento de Ciencias Administrativas (DEPCA)', 2),
+(4, 'Departamento de Estudios Organizacionales Desarrollo Humano (DESODEH)', 2),
+(5, 'Departamento de Ingeniería Civil y Ambiental (DICA)', 3),
+(6, 'Departamento de Automatización y Control Industrial (DACI)', 4),
+(7, 'Departamento de Energía Eléctrica (DEE)', 4),
+(8, 'Departamento de Electrónica, Telecomunicaciones y Redes de Información (DETRI)', 4),
+(9, 'Departamento de Geología (DG)', 5),
+(10, 'Departamento de Petróleos (DP)', 5),
+(11, 'Departamento de Ingenieria Mecanica (DIM)', 6),
+(12, 'Departamento de Materiales (DMT)', 6),
+(13, 'Departamento de Ingeniería Química (DIQ)', 7),
+(14, 'Departamento de Ciencias de Alimentos y Biotecnología (DECAB)', 7),
+(15, 'Departamento de Ciencias Nucleares (DCN)', 7),
+(16, 'Departamento de Metalurgia Extractiva (DEMEX)', 7),
+(17, 'Departamento de Informática y Ciencias de la Computación (DICC)', 8),
+(18, 'Departamento de Formacion Basica (DFB)', 10),
+(19, 'Instituto Geofisico', 10),
+(20, 'Departamento de Ciencias Sociales', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `idEstudiante` int(11) NOT NULL,
+  `cedulaEst` varchar(10) NOT NULL,
+  `nombresEst` varchar(50) NOT NULL,
+  `apellidosEst` varchar(50) NOT NULL,
+  `correoEst` varchar(50) NOT NULL,
+  `idCarrera` int(11) NOT NULL,
+  `usuarioEst` varchar(15) NOT NULL,
+  `pwEst` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `estudiante`
+--
+
+INSERT INTO `estudiante` (`idEstudiante`, `cedulaEst`, `nombresEst`, `apellidosEst`, `correoEst`, `idCarrera`, `usuarioEst`, `pwEst`) VALUES
+(2, '1231231232', 'Fran', 'Iz', 'asdad@sc.com', 20, 'fraz', '$2y$10$VW9..wJnd7Gk12zKoqVDdeuh2XBJlqrW5NChxLdmqMsaIyB1OdsUS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facultad`
+--
+
+CREATE TABLE `facultad` (
+  `idFacultad` int(11) NOT NULL,
+  `nombreFacultad` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `facultad`
+--
+
+INSERT INTO `facultad` (`idFacultad`, `nombreFacultad`) VALUES
+(1, 'Facultad de Ciencias'),
+(2, 'Facultad de Ciencias Administrativas'),
+(3, 'Facultad de Ing. Civil y Ambiental'),
+(4, 'Facultad de Ing. Electrica y Electronica'),
+(5, 'Facultad de Geologia y Petroleos'),
+(6, 'Facultad de Ing. Mecanica'),
+(7, 'Facultad de Ing. Quimica y Agroindustria'),
+(8, 'Facultad de Ing. de Sistemas'),
+(9, 'Escuela de Formacion de Tecnologos'),
+(10, 'Otros');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `objetoaprendizaje`
 --
 
 CREATE TABLE `objetoaprendizaje` (
-  `id` int(11) NOT NULL,
+  `idOA` int(11) NOT NULL,
   `ruta` varchar(200) NOT NULL,
   `nombre` varchar(50) CHARACTER SET utf8 NOT NULL,
   `autor` varchar(100) CHARACTER SET utf8 NOT NULL,
   `descripcion` varchar(1000) CHARACTER SET utf8 NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha` date NOT NULL,
   `p_clave` varchar(200) CHARACTER SET utf8 NOT NULL,
   `institucion` varchar(100) CHARACTER SET utf8 NOT NULL,
   `tamano` varchar(100) NOT NULL,
   `tipo` varchar(10) NOT NULL,
-  `fecha_ing` date NOT NULL,
-  `ruta_zip` varchar(200) NOT NULL
+  `fecha_ing` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ruta_zip` varchar(200) NOT NULL,
+  `idProfesor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `objetoaprendizaje`
 --
 
-INSERT INTO `objetoaprendizaje` (`id`, `ruta`, `nombre`, `autor`, `descripcion`, `fecha`, `p_clave`, `institucion`, `tamano`, `tipo`, `fecha_ing`, `ruta_zip`) VALUES
-(1, 'oa/metodos_desarrollo/index.html', 'Metodos de Desarrollo', 'Francisco Izurieta', 'Los modelos de desarrollo de software son una representaci?n abstracta de una manera en particular. Realmente no representa c?mo se debe desarrollar el software, sino de un enfoque com?n.', '2017-12-09 22:29:10', 'metodos, desarrollo', 'Escuela Politecnica Nacional', '361575 Bytes', 'Zip', '2018-01-16', 'metodos_desarrollo.zip'),
-(2, 'oa/patrones_diseno/index.html', 'Patrones de Diseño', 'Dario Herrera, Alexis Guerrero, Francisco Izurieta', 'En la ingeniería de software existen distintas técnicas para poder desarrollar programas funcionales. No se puede afirmar o decir cu?l es la mejor t?cnica para poder desarrollar software, ya que cada t?cnica se apega a una necesidad distinta dependiendo del desarrollador.', '2018-01-09 22:31:26', 'diseño, patrones', 'Escuela Politecnica Nacional', '1367454 Bytes', 'Zip', '0000-00-00', 'patrones_diseno.zip'),
-(3, '', 'Diseño de Interfaz de Usuario', 'Francisco Izurieta', 'El diseño de la interfaz de usuario crea un medio eficaz de comunicación entre los seres humanos y la computadora.', '2018-01-09 22:32:43', 'diseño, interfaz, usuario', 'Escuela Politecnica Nacional', '958856 Bytes', 'Zip', '0000-00-00', 'diseno_interfaces.zip'),
-(4, '', 'Diseño arquitectónico del software', 'Graciela Moreno, Paola Guamani, Gabriel Macias', '', '2018-01-09 22:33:45', 'diseño, arquitectura, software', 'Escuela Politecnica Nacional', '154975 Bytes', 'Zip', '0000-00-00', 'diseno_arquitectura.zip'),
-(5, '', 'Diseño a nivel de componentes', 'Andrea Villacis, Carlos Gutierrez', 'El diseño en el nivel de componentes define las estructuras de datos, algoritmos, caracteristicas de la interfaz y mecanismos de comunicacion asignadas a cada componente.', '2018-01-09 22:34:27', 'diseño, componentes', 'Escuela Politecnica Nacional', '525129 Bytes', 'Zip', '0000-00-00', 'diseno_componentes.zip');
+INSERT INTO `objetoaprendizaje` (`idOA`, `ruta`, `nombre`, `autor`, `descripcion`, `fecha`, `p_clave`, `institucion`, `tamano`, `tipo`, `fecha_ing`, `ruta_zip`, `idProfesor`) VALUES
+(2, '', 'Diseño de la arquitectura de software', 'Graciela Moreno, Paola Guamanì, Gabriel Macias', 'Crear un objeto de aprendizaje que ayude como material de apoyo, o como recurso para un aprendizaje autodidacta de los diseños de arquitectura de software.', '2017-12-12', 'diseño, arquitectura', 'Escuela Politecnica Nacional', '154226 bytes', 'WinRAR ZIP', '2018-01-28 20:08:09', 'diseno_arquitectura.zip', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profesor`
+--
+
+CREATE TABLE `profesor` (
+  `idProfesor` int(11) NOT NULL,
+  `cedulaProf` varchar(10) NOT NULL,
+  `nombresProf` varchar(50) NOT NULL,
+  `apellidosProf` varchar(50) NOT NULL,
+  `correoProf` varchar(50) NOT NULL,
+  `idDepartamento` int(11) NOT NULL,
+  `usuarioProf` varchar(15) NOT NULL,
+  `pwProf` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `profesor`
+--
+
+INSERT INTO `profesor` (`idProfesor`, `cedulaProf`, `nombresProf`, `apellidosProf`, `correoProf`, `idDepartamento`, `usuarioProf`, `pwProf`) VALUES
+(3, '', 'adsa', 'asdas', 'hawupux@69postix.info', 6, 'a', '$2y$10$5Mc0tltLELeaaq/2O9E.1.5A94dnSJPPGaI/uKxd6B8sRSoQREMAi'),
+(6, '1231231231', 'Francisco ', 'Izurieta', 'franizus@live.com', 17, 'frna', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`idAdministrador`);
+
+--
+-- Indexes for table `carrera`
+--
+ALTER TABLE `carrera`
+  ADD PRIMARY KEY (`idCarrera`),
+  ADD KEY `idFacultad` (`idFacultad`);
+
+--
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `idOA` (`idOA`),
+  ADD KEY `idProfesor` (`idProfesor`);
+
+--
+-- Indexes for table `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`idDepartamento`),
+  ADD KEY `idFacultad` (`idFacultad`);
+
+--
+-- Indexes for table `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`idEstudiante`),
+  ADD KEY `idCarrera` (`idCarrera`);
+
+--
+-- Indexes for table `facultad`
+--
+ALTER TABLE `facultad`
+  ADD PRIMARY KEY (`idFacultad`);
+
+--
 -- Indexes for table `objetoaprendizaje`
 --
 ALTER TABLE `objetoaprendizaje`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`idOA`),
+  ADD KEY `idProfesor` (`idProfesor`);
+
+--
+-- Indexes for table `profesor`
+--
+ALTER TABLE `profesor`
+  ADD PRIMARY KEY (`idProfesor`),
+  ADD KEY `idDepartamento` (`idDepartamento`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `idAdministrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `carrera`
+--
+ALTER TABLE `carrera`
+  MODIFY `idCarrera` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `idDepartamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `estudiante`
+--
+ALTER TABLE `estudiante`
+  MODIFY `idEstudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `facultad`
+--
+ALTER TABLE `facultad`
+  MODIFY `idFacultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `objetoaprendizaje`
 --
 ALTER TABLE `objetoaprendizaje`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idOA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `profesor`
+--
+ALTER TABLE `profesor`
+  MODIFY `idProfesor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `carrera`
+--
+ALTER TABLE `carrera`
+  ADD CONSTRAINT `carrera_ibfk_1` FOREIGN KEY (`idFacultad`) REFERENCES `facultad` (`idFacultad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`idComentario`) REFERENCES `objetoaprendizaje` (`idOA`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`idProfesor`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `departamento`
+--
+ALTER TABLE `departamento`
+  ADD CONSTRAINT `departamento_ibfk_1` FOREIGN KEY (`idFacultad`) REFERENCES `facultad` (`idFacultad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`idCarrera`) REFERENCES `carrera` (`idCarrera`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `objetoaprendizaje`
+--
+ALTER TABLE `objetoaprendizaje`
+  ADD CONSTRAINT `objetoaprendizaje_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`idProfesor`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `profesor`
+--
+ALTER TABLE `profesor`
+  ADD CONSTRAINT `profesor_ibfk_1` FOREIGN KEY (`idDepartamento`) REFERENCES `departamento` (`idDepartamento`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
