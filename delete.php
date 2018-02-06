f6992cf19bb3b3b3aeaf611f2c10b6663a7d27b2
Unknown <franizus@live.com>
2018-02-06 16:22:29 -0500

    changed deleteOA to function

--- delete.php
+++ delete.php
@@ -1,31 +1,29 @@
 <?php
     require_once "pdo.php";
-    session_start();
 
-    $filename = $_POST["ruta_zip"];
-    $name = basename($filename,".zip"); 
-    $target = 'oa/' . $name . '/';
-
-    if(is_dir($target)){
-        $files = glob( $target . '*', GLOB_MARK );
-            
-        foreach( $files as $file )
-        {
-            unlink( $file );
+    function deleteOA($filename, $idOA) {
+        $name = basename($filename,".zip"); 
+        $target = 'oa/' . $name . '/';
+    
+        if(is_dir($target)){
+            $files = glob( $target . '*', GLOB_MARK );
+                
+            foreach( $files as $file )
+            {
+                unlink( $file );
+            }
+              
+            rmdir( $target );
         }
-          
-        rmdir( $target );
+    
+        $dirZip = 'zip/' . $filename;
+        unlink($dirZip);
+    
+        $sql = "DELETE FROM objetoaprendizaje WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+    
+        $sql = "DELETE FROM comentario WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
     }
-
-    $dirZip = 'zip/' . $filename;
-    unlink($dirZip);
-
-    $sql = "DELETE FROM objetoaprendizaje WHERE idOA = :idOA";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':idOA' => $_POST["id"]));
-
-    $sql = "DELETE FROM comentario WHERE idOA = :idOA";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':idOA' => $_POST["id"]));
-    $_SESSION["oa"] = "Objeto de Aprendizaje eliminado del sistema correctamente.";
-?>
